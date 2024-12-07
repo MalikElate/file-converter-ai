d737bbb8197038f1dea1f30436f00f417dd8d8cb
Malik Elate <Malik.e1955@gmail.com>
2024-12-07 13:09:39 -0600

    0.2 dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import clerk from '@clerk/astro';
-import vercel from '@astrojs/vercel/serverless';
-import partytown from '@astrojs/partytown'
+// import clerk from "@clerk/astro";
+// import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
+
+import db from "@astrojs/db";
 
 export default defineConfig({
-  output: 'server',
-  site: 'https://astro-article.netlify.app/',
+  output: "server",
+  site: "https://astro-article.netlify.app/",
   adapter: vercel({
     analytics: true,
     maxDuration: 60,
@@ -17,12 +19,13 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    clerk(), 
+    clerk(),
     partytown({
-        config: {
-          forward: ["dataLayer.push"],
-        },
+      config: {
+        forward: ["dataLayer.push"],
+      },
     }),
+    db(),
   ],
   build: {
     minify: true,
